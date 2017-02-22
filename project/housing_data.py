5e0c459a4299ff4e6e7ba28a2d574c6dc76ba72e
BrandonSpitler <brandonfspitler@gmail.com>
2017-02-22 13:21:51 -0700

    complete change to panda

--- project/housing_data.py
+++ project/housing_data.py
@@ -1,36 +1,14 @@
 import csv
 import numpy
+from sklearn import datasets, linear_model
+import pandas
+from statsmodels.formula.api import ols
+data = pandas.read_csv('project/train.csv',sep=',')
 
-def type_convert(type,value):
-    if (type == 's'):
-        return (value)
-    elif (value == 'NA'):
-        return(None)
-    elif (type == 'i'):
-        return int(value)
-    elif (type == 'f'):
-        return float(value)
-    return 1
 
-housing_data_file = open('../train.csv')
-housing_Data = csv.reader(housing_data_file)
-i = 0
-header = next(housing_Data)
-data = list(housing_Data)
-header_length = len(header)
-
-rows_to_keep = [4,12,80,77]
-length_of_rows_to_keep = len(rows_to_keep)
-conversion_array = ['i','s','i','s']
-length_of_data = len(data)
-
-data_file = numpy.empty((length_of_data,length_of_rows_to_keep), dtype = numpy.object)
-
-for i in range(0,length_of_data):
-    for n in range(0,length_of_rows_to_keep):
-       index = rows_to_keep[n]
-       converter = type_convert(conversion_array[n],data[i][index])
-       data_file[i][n] = converter
-
+#subseting example
+#t = data[data['YrSold']==2009] 
+model = ols("SalePrice ~ LotArea",data).fit()
+print(model.summary())
 print("done")
 
---
+++ project/reg_tree.py
@@ -0,0 +1,12 @@
+
+import numpy
+from sklearn import datasets, linear_model
+
+#class reg_tree(data_file,indexs,y_variable):
+#    housing_data_fit = linear_model.LinearRegression()
+#    children = {}
+#    def set_child(sub_reg_tree, hash_value):
+#        children[hash_value] = sub_reg_tree
+#    def get_child(hash_value):
+#        return(children[hash])
+#    def set_linear_equation():
