4315f6959618a448dfe2cf044ab39c1dbfa9f6b4
drewmeyersCUboulder <drew.meyers@colorado.edu>
2017-02-22 13:48:27 -0700

    added linear regression plot with matplotlib

--- project/housing_data.py
+++ project/housing_data.py
@@ -1,36 +1,14 @@
 import csv
 import numpy
+import matplotlib.pyplot as plt
+import pandas as pd
 
-def type_convert(type,value):
-    if (type == 's'):
-        return (value)
-    elif (value == 'NA'):
-        return(None)
-    elif (type == 'i'):
-        return int(value)
-    elif (type == 'f'):
-        return float(value)
-    return 1
+housing_Data = pd.read_csv('train.csv', sep=",")
+#print housing_Data.columns
+#print housing_Data['SalePrice'].describe()
 
-housing_data_file = open('../train.csv')
-housing_Data = csv.reader(housing_data_file)
-i = 0
-header = next(housing_Data)
-data = list(housing_Data)
-header_length = len(header)
-
-rows_to_keep = [4,12,80,77]
-length_of_rows_to_keep = len(rows_to_keep)
-conversion_array = ['i','s','i','s']
-length_of_data = len(data)
-
-data_file = numpy.empty((length_of_data,length_of_rows_to_keep), dtype = numpy.object)
-
-for i in range(0,length_of_data):
-    for n in range(0,length_of_rows_to_keep):
-       index = rows_to_keep[n]
-       converter = type_convert(conversion_array[n],data[i][index])
-       data_file[i][n] = converter
-
+var = 'OverallQual'
+data = pd.concat([housing_Data['SalePrice'], housing_Data[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+plt.show()
 print("done")
-
